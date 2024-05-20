eded7e7882b2b1f3fd7b92656e4069a3db1bf9de
Gifty Arthur <giftyarthur133@gmail.com>
2024-05-20 19:43:29 +0000

    components

---
+++ src/Components/Product/step1.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const step1 = () => {
+  return <div></div>;
+};
+
+export default step1;
---
+++ src/Components/Product/step3.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const step3 = () => {
+  return <div></div>;
+};
+
+export default step3;
