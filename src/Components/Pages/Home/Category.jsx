464b7a3502b93ad73c237b78390aca0af6da5ade
Gifty Arthur <giftyarthur031@gmail.com>
2024-06-05 17:09:01 +0000

    category

--- src/Components/Pages/Home/Category.jsx
+++ src/Components/Pages/Home/Category.jsx
@@ -1,96 +1,93 @@
 import React from "react";
-import { Link } from "react-router-dom";
-// Mobile imgaes
-import mbanner3 from "../../../assets/Images/mbanner3.png";
-import mbanner4 from "../../../assets/Images/mbanner4.png";
-import mbanner1 from "../../../assets/Images/mbanner1.png";
-import mbanner2 from "../../../assets/Images/mbanner2.png";
 
-//web images
+// images
 import banner1 from "../../../assets/Images/banner1.png";
+import banner3 from "../../../assets/Images/banner3.png";
+import banner4 from "../../../assets/Images/banner4.png";
+import banner2 from "../../../assets/Images/banner2.png";
 
-function Category() {
+const Category = () => {
   return (
-    <div className="w-full ">
+    <div className=" max-w-screen-2xl md:h-[600px] ">
       <div className="container">
         <div>
-          {/* img1 */}
-          <div className="md:flex flex-col-reverse">
-            <div className="bg-white w-full  mt-10 ">
-              <img className="w-[192px] ml-20 " src={mbanner3} alt="" />
-              <div className="flex  flex-wrap gap-2 mt-8 ml-8">
-                <h1 className="text-[34px] font-light ">Apple AirPods</h1>
-                <h1 className="text-[34px] font-medium  ">Max</h1>
-              </div>
-              <p className="text-calm font-medium text-md ml-2">
-                Computational a16pxudio. Listen, it's powerful
-              </p>
-            </div>
-            {/* img2 */}
-            <div className="bg-mid w-full h-96 ">
-              <img className="ml-10 mt-10 w-[325px] " src={mbanner4} alt="" />
-              <div className="flex  flex-wrap gap-2 mt-4 ml-14">
-                <h1 className="text-[34px] font-light text-white ">
-                  Apple Vision
+          <div className=" bg-white md:h-[328px] md:w-[720px] ">
+            <div className="flex ">
+              <img className="md:w-[360px] md:h-[343px]" src={banner1} alt="" />
+              <div className="flex flex-col items-start justify-center mt-4  ">
+                <h1 className="font-medium text-3xl font-roboto sm:text-4xl">
+                  Playstation 5
                 </h1>
-                <h1 className="text-[34px] font-medium text-white ">Pro</h1>
+                <p className="text-md font-medium text-calm ">
+                  Incredibly powerful CPUs, GPUs, and an SSD with <br />
+                  <span>
+                    integrated I/O will redefine your PlayStation experience.
+                  </span>
+                </p>
               </div>
-              <p className="text-calm font-medium text-md ml-2">
-                An immersive way to experience entertainments
-              </p>
-            </div>
-            {/* img3 */}
-            <div className="bg-white w-full h-[424px] md:w-[720px] md:h-[328px] md:flex mt-6">
-              <img
-                className=" md:hidden md:ml-16 md:h-[360px] md:w-[343px]"
-                src={mbanner1}
-                alt=""
-              />
-
-              <div className="flex flex-wrap gap-2 mt-4 ml-14 md:flex-wrap">
-                <h1 className="text-[34px] font-regular text-black ml-4 md:text-[49px]">
-                  Playstation
-                </h1>
-                <h1 className="text-[34px] font-medium text-black md:text-[49px]">
-                  5
-                </h1>
-              </div>
-              <p className="text-calm font-medium text-md ml-2 center-text mt-2">
-                Incredibly powerful CPUs, GPUs, and an SSD with integrated I/O
-                <span className="centered-line">
-                  will redefine your PlayStation experience.
-                </span>
-              </p>
             </div>
           </div>
 
-          {/* img4 */}
-
-          <div className="bg-picker h-[504px]">
-            <img className="ml-8 w-[330px] " src={mbanner2} alt="" />
-            <div className="flex  flex-wrap gap-2 mt-4 ml-14">
-              <h1 className="text-[34px] font-medium text-black ml-4  ">
-                Macbook
-              </h1>
-              <h1 className="text-[34px] font-light text-black ">Air</h1>
+          {/* headphone */}
+          <div className="flex ">
+            <div className="bg-picker md:w-[360px] md:h-[272px] items-center ">
+              <div className="flex ">
+                <img
+                  className="md:w-[104px] md:h-[272px]"
+                  src={banner3}
+                  alt=""
+                />
+                <div className="flex flex-col items-start justify-center ml-8   ">
+                  <h1 className="font-light text-3xl  sm:text-[29px]">
+                    Apple <br /> AirPods
+                  </h1>
+                  <h1 className="font-medium font-roboto text-3xl sm:text-[29px]">
+                    Max
+                  </h1>
+                  <p className="text-md font-medium text-calm ">
+                    Computational audio
+                    <br /> Listen, it's powerful
+                  </p>
+                </div>
+              </div>
             </div>
-            <p className="text-calm font-medium text-md ml-2 mt-2">
-              The new 15‑inch MacBook Air makes room for more of what you love
-              with a spacious Liquid Retina display.
-            </p>
 
-            <Link to="/shop">
-              <button className="w-[343px] h-[56px] mt-8 ml-6  rounded-md border-black  border-2 text-black  hover:bg-violet-500 ">
-                Shop Now
-              </button>
-            </Link>
+            {/* vision  */}
+
+            <div className="md:w-[360px] h-[272px] bg-mid flex items-center justify-center">
+              <div className="flex mt-14">
+                <img
+                  className="md:w-[136px] md:h-[190px]"
+                  src={banner4}
+                  alt=""
+                />
+                <div className="flex flex-col items-start justify-center   ml-8">
+                  <h1 className="font-light text-3xl text-white  sm:text-[29px]">
+                    Apple <br /> Vision{" "}
+                    <span className="sm:text-[29px] font-medium font-roboto">
+                      Pro
+                    </span>
+                  </h1>
+
+                  <p className="text-md font-medium text-calm mt-2 ">
+                    An immersive way to experience
+                    <br /> entertainment
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
 
-          {/* browse */}
+        {/* laptop */}
+        <div>
+          <div>
+            <img src={banner2} alt="" />
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Category;
