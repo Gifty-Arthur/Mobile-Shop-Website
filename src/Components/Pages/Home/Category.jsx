806eaae6f2901bbed5d30a302c415d28f562518d
Gifty Arthur <giftyarthur031@gmail.com>
2024-06-06 19:08:20 +0000

    changes

--- src/Components/Pages/Home/Category.jsx
+++ src/Components/Pages/Home/Category.jsx
@@ -8,83 +8,9 @@ import banner2 from "../../../assets/Images/banner2.png";
 
 const Category = () => {
   return (
-    <div className=" max-w-screen-2xl md:h-[600px] ">
-      <div className="container">
-        <div>
-          <div className=" bg-white md:h-[328px] md:w-[720px] ">
-            <div className="flex ">
-              <img className="md:w-[360px] md:h-[343px]" src={banner1} alt="" />
-              <div className="flex flex-col items-start justify-center mt-4  ">
-                <h1 className="font-medium text-3xl font-roboto sm:text-4xl">
-                  Playstation 5
-                </h1>
-                <p className="text-md font-medium text-calm ">
-                  Incredibly powerful CPUs, GPUs, and an SSD with <br />
-                  <span>
-                    integrated I/O will redefine your PlayStation experience.
-                  </span>
-                </p>
-              </div>
-            </div>
-          </div>
-
-          {/* headphone */}
-          <div className="flex ">
-            <div className="bg-picker md:w-[360px] md:h-[272px] items-center ">
-              <div className="flex ">
-                <img
-                  className="md:w-[104px] md:h-[272px]"
-                  src={banner3}
-                  alt=""
-                />
-                <div className="flex flex-col items-start justify-center ml-8   ">
-                  <h1 className="font-light text-3xl  sm:text-[29px]">
-                    Apple <br /> AirPods
-                  </h1>
-                  <h1 className="font-medium font-roboto text-3xl sm:text-[29px]">
-                    Max
-                  </h1>
-                  <p className="text-md font-medium text-calm ">
-                    Computational audio
-                    <br /> Listen, it's powerful
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            {/* vision  */}
-
-            <div className="md:w-[360px] h-[272px] bg-mid flex items-center justify-center">
-              <div className="flex mt-14">
-                <img
-                  className="md:w-[136px] md:h-[190px]"
-                  src={banner4}
-                  alt=""
-                />
-                <div className="flex flex-col items-start justify-center   ml-8">
-                  <h1 className="font-light text-3xl text-white  sm:text-[29px]">
-                    Apple <br /> Vision{" "}
-                    <span className="sm:text-[29px] font-medium font-roboto">
-                      Pro
-                    </span>
-                  </h1>
-
-                  <p className="text-md font-medium text-calm mt-2 ">
-                    An immersive way to experience
-                    <br /> entertainment
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* laptop */}
-        <div>
-          <div>
-            <img src={banner2} alt="" />
-          </div>
-        </div>
+    <div className="   ">
+      <div className="">
+        <div>{/* <img src={banner1} alt="" /> */}</div>
       </div>
     </div>
   );
--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,39 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import iphone from "../../../assets/Images/iphone.png";
-import Lottie from "lottie-react";
-import animationData from "../../../assets/Images/animation.json";
 
 function Home() {
   return (
-    <div className="bg-head w-full h-auto md:flex md:justify-center">
-      <div className="container md:flex md:flex-row md:items-center">
-        <div className=" md:mt-0 md:order-1">
-          <h3 className="text-white text-xl md:text-[25px] font-semibold opacity-[50%] mt-2 ml-32 md:ml-0">
-            Pro.Beyond.
-          </h3>
-          <div className="md:flex md:flex-wrap">
-            <h1 className="text-white text-[4.5rem] ml-10 md:ml-0 font-light">
-              IPhone 14
-            </h1>
-            <p className="text-white text-[4.5rem] font-semibold font-roboto ml-24 md:ml-2 mt-2">
-              Pro
-            </p>
+    <div className="bg-head h-auto ">
+      <div className="container ">
+        <div className=" md:my-8 flex flex-col md:flex-row-reverse md:items-center justify-between ">
+          <img className=" md:[632px] sm:flex hidden " src={iphone} alt="" />
+          <div className="md:w-1/2 mt-10 ">
+            <div className="ml-20 ">
+              <h2 className="text-white opacity-50 text-md ml-12 md:ml-0 ">
+                Pro.Beyond
+              </h2>
+              <h1 className="text-white text-5xl md:text-6xl font-thin  ">
+                IPhone 14{" "}
+                <span className="font-medium flex ml-12 md:ml-0 md:inline">
+                  Pro
+                </span>
+              </h1>
+              <p className="text-white opacity-50 mt-2">
+                Created to change everything for the better. For everyone
+              </p>
+            </div>
+            <Link to="/shop">
+              <button className="w-[184px] h-[56px] mt-6 md:ml-20 ml-20  rounded-md border border-1 text-white hover:bg-violet-500">
+                Shop Now
+              </button>
+            </Link>{" "}
           </div>
-          <p className="text-[#909090] text-[19px] font-medium opacity ml-10 md:ml-0">
-            Created to change everything for the better. For everyone
-          </p>
-          <Link to="/shop">
-            <button className="w-[184px] h-[56px] mt-6 ml-20 md:ml-0 rounded-md border border-1 text-white hover:bg-violet-500">
-              Shop Now
-            </button>
-          </Link>
         </div>
-        <div className="md:ml-20 md:order-2">
+        <div>
           <img
-            className="w-80 h-[289px] ml-8 md:w-[406px] md:h-[632px] md:ml-10"
+            className=" flex sm:hidden h-[289px] w-[343px]"
             src={iphone}
-            alt="iphone"
+            alt=""
           />
         </div>
       </div>
